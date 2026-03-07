a4f86b52f5f6de7a832bef7fef295362ab27f328
Chhoshi9181 <Chanathip2961@gmail.com>
2026-03-07 16:18:53 +0700

    a1-001.py

--- a1-001.py
+++ a1-001.py
@@ -1,9 +1,9 @@
-Input:
-Chanathip
-Chasawat
+# รับชื่อและนาสกุล
+Chanathip = input()
+Chasawat = input()
 
 # แสดงบรรทัดที่่ 1
 print(f"Hello {Chanathip} {Chasawat}")
 
 # แสดงบรรทัดที่่ 2
-print(Chanathip[:2]  + Chasawat[:2])
+print(Chanathip[:2] + Chasawat[:2])
