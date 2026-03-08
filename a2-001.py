0117f574f47fcdd9b5940cdb41c9ca441ecbd7ca
Chhoshi9181 <Chanathip2961@gmail.com>
2026-03-08 09:35:30 +0700

    a2-001.py

--- a2-001.py
+++ a2-001.py
@@ -3,31 +3,9 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-red = list(map(int, input().split()))
-blue = list(map(int, input().split()))
+red = set(map(int, input().split()))
+blue = set(map(int, input().split()))
 
-i = j = 0
-dir_r = 1
-dir_b = 1
-ans = 1
+same = len(red & blue)
 
-while i < n or j < m:
-
-    if j == m or (i < n and red[i] < blue[j]):
-        dir_r *= -1
-        i += 1
-
-    elif i == n or (j < m and blue[j] < red[i]):
-        dir_b *= -1
-        j += 1
-
-    else:
-        dir_r *= -1
-        dir_b *= -1
-        i += 1
-        j += 1
-
-    if dir_r != dir_b:
-        ans += 1
-
-print(ans)
+print(n + m - same - 2)
