0638a187996a7fe380d66f9c9497524448038c2d
Chhoshi9181 <Chanathip2961@gmail.com>
2026-03-08 09:15:38 +0700

    a2-001.py

--- a2-001.py
+++ a2-001.py
@@ -4,24 +4,20 @@ red = list(map(int, input().split()))
 blue = list(map(int, input().split()))
 
 i = j = 0
-dir_r = 1
-dir_b = 1
-ans = 1   # นับจุดเริ่มต้น
+last = None
+count = 1   # นับจุดเริ่มต้น
 
-while i < n and j < m:
-    if red[i] < blue[j]:
-        dir_r *= -1
+while i < n or j < m:
+    if j == m or (i < n and red[i] < blue[j]):
+        cur = 'r'
         i += 1
-    elif red[i] > blue[j]:
-        dir_b *= -1
-        j += 1
     else:
-        dir_r *= -1
-        dir_b *= -1
-        i += 1
+        cur = 'b'
         j += 1
     
-    if dir_r != dir_b:
-        ans += 1
+    if last is not None and cur != last:
+        count += 1
+    
+    last = cur
 
-print(ans)
+print(count)
