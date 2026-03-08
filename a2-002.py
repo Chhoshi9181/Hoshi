cf69d07a1ee0a6f45518952beb3981cc5990d362
Chhoshi9181 <Chanathip2961@gmail.com>
2026-03-08 09:21:15 +0700

    a2-002.py

--- a2-002.py
+++ a2-002.py
@@ -1,9 +1,15 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 
 points = []
+s = set()
+
 for _ in range(n):
     x, y = map(int, input().split())
     points.append((x, y))
+    s.add((x, y))
 
 ans = 0
 
@@ -11,8 +17,9 @@ for i in range(n):
     x1, y1 = points[i]
     for j in range(i+1, n):
         x2, y2 = points[j]
-        
+
         if abs(x1-x2) == abs(y1-y2):
-            ans = max(ans, abs(x1-x2))
+            if (x1, y2) in s and (x2, y1) in s:
+                ans = max(ans, abs(x1-x2))
 
 print(ans)
