21f4ef84514444184d3eaebdc4766b89a80c7610
Chhoshi9181 <Chanathip2961@gmail.com>
2026-03-08 09:25:54 +0700

    a2-001.py

--- a2-001.py
+++ a2-001.py
@@ -1,23 +1,33 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 
 red = list(map(int, input().split()))
 blue = list(map(int, input().split()))
 
 i = j = 0
-last = None
-count = 1   # นับจุดเริ่มต้น
+dir_r = 1
+dir_b = 1
+ans = 1
 
 while i < n or j < m:
-    if j == m or (i < n and red[i] < blue[j]):
-        cur = 'r'
-        i += 1
-    else:
-        cur = 'b'
-        j += 1
-    
-    if last is not None and cur != last:
-        count += 1
-    
-    last = cur
 
-print(count)
+    if j == m or (i < n and red[i] < blue[j]):
+        dir_r *= -1
+        i += 1
+
+    elif i == n or (j < m and blue[j] < red[i]):
+        dir_b *= -1
+        j += 1
+
+    else:
+        dir_r *= -1
+        dir_b *= -1
+        i += 1
+        j += 1
+
+    if dir_r != dir_b:
+        ans += 1
+
+print(ans)
