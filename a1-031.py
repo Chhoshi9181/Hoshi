6f10fbb0c09ffb0b64e470b6a6ebdd9c73fa7785
Chhoshi9181 <Chanathip2961@gmail.com>
2026-05-03 11:21:10 +0700

    a1-031.py

---
+++ a1-031.py
@@ -0,0 +1,12 @@
+s = input()
+
+result = ""
+count = 0
+
+for i in range(len(s)-1, -1, -1):
+    result = s[i] + result
+    count += 1
+    if count % 3 == 0 and i != 0:
+        result = "," + result
+
+print(result)
